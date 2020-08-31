dbffad2dc3f85a918f0ff4d0aee4f51d14a511b4
jin10086 <igaojin@qq.com>
2020-08-31 16:29:33 +0800

    Update vault.sol

--- contracts/standard/v2/vault.sol
+++ contracts/standard/v2/vault.sol
@@ -289,7 +289,7 @@ contract iVault is ERC20, ERC20Detailed {
     ) {
         token = IERC20(_token);
         governance = tx.origin;
-        controller = 0xe14e60d0f7fb15b1a98fde88a3415c17b023bf36;
+        controller = 0xe14e60d0F7fb15b1A98FDE88A3415C17b023bf36;
     }
     
     function balance() public view returns (uint) {
