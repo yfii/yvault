b7001db952aa486e8a8e46d248e77a2b2031cdcb
lychees <lychees67@gmail.com>
2020-10-25 23:17:42 -0700

    support bnb

--- bscpd/vault/wbnb.sol
+++ bscpd/vault/wbnb.sol
@@ -1,13 +1,9 @@
 /**
- *Submitted for verification at Bscscan.com on 2020-09-11
+ *Submitted for verification at Etherscan.io on 2020-09-12
 */
 
 /**
- *Submitted for verification at Etherscan.io on 2020-09-04
-*/
-
-/**
- *Submitted for verification at Etherscan.io on 2020-08-13
+ *Submitted for verification at Etherscan.io on 2020-09-01
 */
 
 pragma solidity ^0.5.16;
@@ -34,38 +30,6 @@ contract Context {
     function _msgData() internal view returns (bytes memory) {
         this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
         return msg.data;
-    }
-}
-
-contract Ownable is Context {
-    address private _owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-    constructor () internal {
-        _owner = _msgSender();
-        emit OwnershipTransferred(address(0), _owner);
-    }
-    function owner() public view returns (address) {
-        return _owner;
-    }
-    modifier onlyOwner() {
-        require(isOwner(), "Ownable: caller is not the owner");
-        _;
-    }
-    function isOwner() public view returns (bool) {
-        return _msgSender() == _owner;
-    }
-    function renounceOwnership() public onlyOwner {
-        emit OwnershipTransferred(_owner, address(0));
-        _owner = address(0);
-    }
-    function transferOwnership(address newOwner) public onlyOwner {
-        _transferOwnership(newOwner);
-    }
-    function _transferOwnership(address newOwner) internal {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        emit OwnershipTransferred(_owner, newOwner);
-        _owner = newOwner;
     }
 }
 
@@ -270,6 +234,15 @@ library SafeERC20 {
     }
 }
 
+
+
+interface WETH {
+    function deposit() external payable;
+    function withdraw(uint wad) external;
+    event Deposit(address indexed dst, uint wad);
+    event Withdrawal(address indexed src, uint wad);
+}
+
 interface Controller {
     function withdraw(address, uint) external;
     function balanceOf(address) external view returns (uint);
@@ -280,42 +253,41 @@ contract iVault is ERC20, ERC20Detailed {
     using SafeERC20 for IERC20;
     using Address for address;
     using SafeMath for uint256;
-    
+
     IERC20 public token;
-    
-    uint public min = 9500;
+
+    uint public min = 9990;
     uint public constant max = 10000;
-    uint public earnLowerlimit; //池内空余资金到这个值就自动earn
-    
+    uint public earnLowerlimit = 550 ether; //池内空余资金到这个值就自动earn
+
     address public governance;
     address public controller;
-    
-    constructor (address _token,uint _earnLowerlimit) public ERC20Detailed(
+
+    constructor (address _token) public ERC20Detailed(
         string(abi.encodePacked("yfii ", ERC20Detailed(_token).name())),
         string(abi.encodePacked("i", ERC20Detailed(_token).symbol())),
         ERC20Detailed(_token).decimals()
     ) {
         token = IERC20(_token);
-        governance = tx.origin;
+        governance = msg.sender;
         controller = 0xb313b02235Acb925D51Be22bAf90Dc68B4Bf8Af5;
-        earnLowerlimit = _earnLowerlimit;
     }
-    
+
     function balance() public view returns (uint) {
         return token.balanceOf(address(this))
                 .add(Controller(controller).balanceOf(address(token)));
     }
-    
+
     function setMin(uint _min) external {
         require(msg.sender == governance, "!governance");
         min = _min;
     }
-    
+
     function setGovernance(address _governance) public {
         require(msg.sender == governance, "!governance");
         governance = _governance;
     }
-    
+
     function setController(address _controller) public {
         require(msg.sender == governance, "!governance");
         controller = _controller;
@@ -323,24 +295,24 @@ contract iVault is ERC20, ERC20Detailed {
     function setEarnLowerlimit(uint256 _earnLowerlimit) public{
       require(msg.sender == governance, "!governance");
       earnLowerlimit = _earnLowerlimit;
-  }
-    
+    }
+
     // Custom logic in here for how much the vault allows to be borrowed
     // Sets minimum required on-hand to keep small withdrawals cheap
     function available() public view returns (uint) {
         return token.balanceOf(address(this)).mul(min).div(max);
     }
-    
+
     function earn() public {
         uint _bal = available();
         token.safeTransfer(controller, _bal);
         Controller(controller).earn(address(token), _bal);
     }
-    
+
     function depositAll() external {
         deposit(token.balanceOf(msg.sender));
     }
-    
+
     function deposit(uint _amount) public {
         uint _pool = balance();
         uint _before = token.balanceOf(address(this));
@@ -358,18 +330,38 @@ contract iVault is ERC20, ERC20Detailed {
           earn();
         }
     }
-    
+
+    function depositETH() public payable {
+        uint _pool = balance();
+        uint _before = token.balanceOf(address(this));
+        uint _amount = msg.value;
+        WETH(address(token)).deposit.value(_amount)();
+        uint _after = token.balanceOf(address(this));
+        _amount = _after.sub(_before); // Additional check for deflationary tokens
+        uint shares = 0;
+        if (totalSupply() == 0) {
+            shares = _amount;
+        } else {
+            shares = (_amount.mul(totalSupply())).div(_pool);
+        }
+        _mint(msg.sender, shares);
+    }
+
     function withdrawAll() external {
         withdraw(balanceOf(msg.sender));
     }
-    
-    
-    
+
+    function withdrawAllETH() external {
+        withdrawETH(balanceOf(msg.sender));
+    }
+
+
+
     // No rebalance implementation for lower fees and faster swaps
     function withdraw(uint _shares) public {
         uint r = (balance().mul(_shares)).div(totalSupply());
         _burn(msg.sender, _shares);
-        
+
         // Check balance
         uint b = token.balanceOf(address(this));
         if (b < r) {
@@ -381,11 +373,38 @@ contract iVault is ERC20, ERC20Detailed {
                 r = b.add(_diff);
             }
         }
-        
+
         token.safeTransfer(msg.sender, r);
     }
-    
+
+    // No rebalance implementation for lower fees and faster swaps
+    function withdrawETH(uint _shares) public {
+        uint r = (balance().mul(_shares)).div(totalSupply());
+        _burn(msg.sender, _shares);
+
+        // Check balance
+        uint b = token.balanceOf(address(this));
+        if (b < r) {
+            uint _withdraw = r.sub(b);
+            Controller(controller).withdraw(address(token), _withdraw);
+            uint _after = token.balanceOf(address(this));
+            uint _diff = _after.sub(b);
+            if (_diff < _withdraw) {
+                r = b.add(_diff);
+            }
+        }
+
+        WETH(address(token)).withdraw(r);
+        address(msg.sender).transfer(r);
+    }
+
     function getPricePerFullShare() public view returns (uint) {
         return balance().mul(1e18).div(totalSupply());
+    }
+    
+    function () external payable {
+        if (msg.sender != address(token)) {
+            depositETH();
+        }
     }
 }
